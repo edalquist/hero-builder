a773dfdfdcebb0d84d65c83573f32fafcd82712e
Eric Dalquist <eric.dalquist@gmail.com>
2014-03-06 22:04:21 -0800

    It Works!

--- hero-builder.js
+++ hero-builder.js
@@ -1,12 +1,27 @@
 var heroBuilderApp = angular.module('heroBuilderApp', [ 'ngResource' ]);
  
 
-heroBuilderApp.controller('HeroBuilderCtrl', function ($scope, $resource) {
+heroBuilderApp.controller('HeroBuilderCtrl', function ($scope, $http) {
 
-	var Race = $resource('/data/race');
-
-	var races = Race.query(function() {
+	$http.get('/data/race.json').success(function(races) {
 		$scope.race = races[0];
 		$scope.races = races;
 	});
+
+	$http.get('/data/history.json').success(function(histories) {
+		$scope.histories = histories;
+	});
+
+	$scope.generate = function() {
+		angular.forEach($scope.histories, function(history) {
+			var roll = Math.floor(Math.random() * 100) + 1;
+			for (var i = 0; i < history.options.length; i++) {
+				var option = history.options[i];
+				if (option.min <= roll && option.max >= roll) {
+					history.selected = i;
+					break;
+				}
+			};
+		});
+	};
 });
